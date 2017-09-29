d31f5d2c649ebf6f50a754fa6cb44d7bfbbda7a2
user <user@user-PC>
2017-09-29 10:36:47 +0700

    add_prods 29/09/60

--- content/add_prods.php
+++ content/add_prods.php
@@ -1,8 +1,17 @@
 <script type="text/javascript">
     $(function () {
-        $("#DP_content").append("<div class='form-group' id='pdId'></div>");
-        $("#pdId").append("รหัสครุภัณฑ์ : <INPUT TYPE='text' NAME='pd_id' id='pd_id'>");
-//                    ,$(""));
+        $("#DP_content").append($("<div class='form-group'>หมวดครุภัณฑ์ : <select name='pdgroup' class='form-control select2' id='pdgroup' required></select>")
+                        ,$("<div class='form-group'>ประเภทครุภัณฑ์ : <select name='pdcate' class='form-control select2' id='pdcate' required></select>")
+                        ,$("<div class='form-group'>รหัสครุภัณฑ์ : <INPUT TYPE='text' NAME='pd_id' id='pd_id'></div>")
+                        ,$("<div class='form-group'>เลขครุภัณฑ์ : <INPUT TYPE='text' NAME='num' id='num'></div>")
+                        ,$("<div class='form-group'>เลขครุภัณฑ์ : <INPUT TYPE='text' NAME='num' id='num'></div>"));
+                $("select#pdgroup").append($("<option value=''> เลือกหมวดครุภัณฑ์ </option>"));
+                                $.getJSON('JsonData/group_data.php', function (LR) {
+                                    for (var key in LR) {
+                                        //if(LR[key].group_id==data.detail.group_id){var select='selected';}else{var select='';}
+                                              $("select#combobox2").append($("<option value='"+LR[key].group_id+"' "/*+select+*/"> "+LR[key].group_name+" </option>"));
+                                    }$(".select2").select2();
+                                });            
     });
 </script> 
 <h2 style="color: blue">รายละเอียด/ดำเนินการความเสี่ยง</h2>
