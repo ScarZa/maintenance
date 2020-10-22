923f2308192d4fef0ea6bf8c2bd84a6880c30be3
ScarZ <ScarZ@ScarZ>
2020-10-22 22:47:18 +0700

    update v.2.1 alert 22/10/63

--- js/refresh_repairT.js
+++ js/refresh_repairT.js
@@ -1,12 +1,12 @@
 function RefreshRepairT() {
    $.getJSON('JsonData/count_repT.php', function (data) {
       $("#myTSpan").empty().append($("<a href='#' class='dropdown-toggle' data-toggle='dropdown'>"
-         + "<i style='color: yellow;' class='fa fa-bell-o'></i>"
-         + "<span class='label label-warning'>" + data.alertRepair + "</span></a>")
+         + "<i style='color: purple;' class='fa fa-bell'></i>"
+         + "<span class='label label-info'>" + data.alertRepair + "</span></a>")
          , $("<ul class='dropdown-menu'><li class='header' style='color: red;'><b>คุณมี " + data.alertRepair + " รายการแจ้งซ่อม(ช่าง)</b></li>"
             + "<li><ul id='menu_Talert' class='menu'></ul></li>"
-            + "<li class='footer'><a href='#' id='total_rep'>ดูทั้งหมด</a></li></ul>"));
-      $("#total_rep").attr("onclick", "loadPage('#index_content','content/list_repairT_order.html')");
+            + "<li class='footer'><a href='#' id='total_repT'>ดูทั้งหมด</a></li></ul>"));
+      $("#total_repT").attr("onclick", "loadPage('#index_content','content/list_repairT_order.html')");
       $("");
       $.getJSON('JsonData/detail_repT.php', function (data) { console.log(data)
          for (var key in data) {
@@ -18,7 +18,7 @@ function RefreshRepairT() {
                icon = 'fa fa-flash fa-2x';
                color = 'red';
             }
-            $("#menu_Talert").append($("<li><a href='#' id='recM' data-toggle='modal' data-target='#receiveTModal' data-whatever='" + data[key].repair_id + "'>"
+            $("#menu_Talert").append($("<li><a href='#' id='recM' data-toggle='modal' data-target='#receiveTModal' data-whatever='" + data[key].repairT_id + "'>"
                + "<h5><i class='" + icon + " text-" + color + "'></i> " + data[key].pd_number + ":</h5>"
                + " <p>" + data[key].depName + "(" + data[key].note + ")</p>"
                + " <p>" + data[key].symptom + "...</p>"
